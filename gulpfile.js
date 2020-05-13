6fe38efd0db69282b2d71e8e0e6ee3d4e490d5b0
superpowerr <50291898+superpowerr@users.noreply.github.com>
2020-05-13 19:35:41 +0300

    feat: added sass compiller & link  media for style.sass

--- gulpfile.js
+++ gulpfile.js
@@ -17,9 +17,9 @@ function bs() {
 
 // Compile sass into CSS & auto-inject into browsers
 function serveSass() {
-  return src("/sass/*.sass")
+  return src("./sass/*.sass")
       .pipe(sass())
-      .pipe(dest("/css"))
+      .pipe(dest("./css"))
       .pipe(browserSync.stream());
 };
 
