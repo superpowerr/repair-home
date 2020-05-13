7e8b1ef818e8213977553a686104d50ca236acbb
superpowerr <50291898+superpowerr@users.noreply.github.com>
2020-05-13 14:25:27 +0300

    include gulp & make gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require ('gulp');
+const browserSync = require('browser-sync').create();    
+const cleanCSS  = require('gulp-clean-css');
+const rename = require("gulp-rename");
+
+gulp.task ('hello', function(done) {
+console.log('Привет, мир!');
+done()
+});
+
+// Static server
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+  gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// mincss
+gulp.task('mincss', function() {
+
+  return gulp.src("./css/*.css")
+  
+  .pipe(rename({suffix: ".min"}))
+  
+  .pipe(cleanCSS())
+  
+  .pipe(gulp.dest("app/css"));
+  
+  })
