54aad91ba12f402338ab578b6acfab89b9b518e7
superpowerr <50291898+superpowerr@users.noreply.github.com>
2020-05-27 17:39:05 +0300

    feat: added two block with slider: target & project

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-/*
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toogle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-modalBtn.forEach(element => {
-  element.addEventListener('click', switchModal);
-});
-
-closeBtn.addEventListener('click', switchModal);
-
-});
-*/
-
 $(document).ready(function () {
   var modal = $('.modal'),
     modalBtn = $('[data-toogle=modal]'),
@@ -26,4 +9,67 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next =$('.swiper-button-next');
+  var prev =$('.swiper-button-next');
+  var bullets =$('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10);
+  
+  //swiper-target
+  var mySwiperTarget = new Swiper('.target__swiper-container', {
+    loop: true,
+    effect: 'coverflow',
+    pagination: {
+      el: '.target__pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.target__button-next',
+      prevEl: '.target__button-prev',
+    },
+
+  });
+  var nextTarget = $('.target__button-next');
+  var prevTarget = $('.target__button-prev');
+  var bulletsTarget = $('.target__pagination');
+
+  nextTarget.css('left', prevTarget.width() + 30 + bulletsTarget.width() + 11);
+  bulletsTarget.css('left', prevTarget.width() + 20);
+
+  //-click block__item in target
+  var block = $('.target-block__item');
+  block.on('click', function (evt) {
+    block.removeClass('target-block__item--active');
+    $(this).toggleClass('target-block__item--active');
+  });
+
+  //-click-to-slide
+  var targetBlock = $('.target-block');
+
+  $('.target-block__item').on('click', function () {
+    var index = $(this).data('index');
+    mySwiperTarget[0].slideTo(index);
+    mySwiperTarget[1].slideTo(index);
+  });
+
+  mySwiperTarget[0].on('slideChange', function () {
+    var index = mySwiperTarget[0].realIndex;
+    block.removeClass('target-block__item--active');
+    block.eq(index).addClass('target-block__item--active');
+  });
+
 });
